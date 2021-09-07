356e529387e6fd969050a6cedbd81d29deacb1ad
darshan magare <darshan.magare1234@gmail.com>
2021-09-07 08:40:04 +0530

    My 21 Aug Assignment

---
+++ AccessSpecifier/AccessSpecifier/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+
+namespace AccessSpecifier
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            ASpec cl1 = new ASpec();
+            cl1.demo();
+            //cl1.demo1(); // can not access or call private method out side its namespace
+            //cl1.demo2();   // can not access or call private method out side its namespace
+            //cl1.demo3(); // can not access or call private method out side its namespace
+            //cl1.demo4(); // can not access or call private method out side its namespace
+           
+
+            int a = cl1.publicvar;
+            //int b = c.privatevar; // can not access or call private method out side its classs
+            //int d = c.protectedvar; ///can not access or call private method out side its classs
+            int e = cl1.internalvar;
+            int f = cl1.protectedinternalvar;
+            
+        }
+    }
+}
